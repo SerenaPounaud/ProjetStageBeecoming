537292b2b1998da36a0785207a14e0b611130b8a
SerenaPounaud <Serena.Pounaud@outlook.fr>
2026-06-11 14:33:04 +0200

    @for tickets

--- frontend/src/app/components/tickets/tickets.ts
+++ frontend/src/app/components/tickets/tickets.ts
@@ -1,11 +1,23 @@
+import { DatePipe, LowerCasePipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [DatePipe, NgClass],
   templateUrl: './tickets.html',
   styleUrl: './tickets.css',
 })
 export class Tickets {
+  tickets:any = [
+    { id: 1, titre: 'Problème de connexion', status: 'Ouvert', date: new Date() },
+    { id: 2, titre: 'Bug interface mobile', status: 'En cours', date: new Date() },
+    { id: 3, titre: 'Demande de remboursement', status: 'Fermé', date: new Date() }
+  ];
 
+  getStatusStyle(status: string): string {
+    if (status.toLowerCase() === 'ouvert') return 'status-open';
+    if (status.toLowerCase() === 'en cours') return 'status-progress';
+    if (status.toLowerCase() === 'fermé') return 'status-closed';
+    return '';
+  }
 }
