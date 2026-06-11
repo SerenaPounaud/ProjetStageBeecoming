421fbd3af7643972ed764a934e1674dda34ddb4b
SerenaPounaud <Serena.Pounaud@outlook.fr>
2026-06-11 16:13:51 +0200

    Gestion inscription en local

--- frontend/src/app/components/sign-up/sign-up.ts
+++ frontend/src/app/components/sign-up/sign-up.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
 export class SignUp {
+  signUpForm !: FormGroup;
+  users:any[]=[];
 
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit():void{
+    this.signUpForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]]
+    })
+  }
+  signUp() {
+    if(this.signUpForm.valid){
+      this.users = JSON.parse(localStorage.getItem('users') || '[]');
+      this.users.push(this.signUpForm.value);
+      localStorage.setItem('users', JSON.stringify(this.users));
+      alert('Inscription réussie');
+      this.signUpForm.reset();
+    }
+  }
 }
