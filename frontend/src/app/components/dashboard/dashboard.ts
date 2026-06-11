f7837accb3f045f1239e7200661becc3f9dd6bec
SerenaPounaud <Serena.Pounaud@outlook.fr>
2026-06-11 10:32:04 +0200

    Ajout affichage dynamique dashboard

--- frontend/src/app/components/dashboard/dashboard.ts
+++ frontend/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Tickets } from '../tickets/tickets';
+import { CreateTicket } from '../create-ticket/create-ticket';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Tickets, CreateTicket],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  sectionActive: string = 'tickets';
+
+  changeSection(section: string) {
+    this.sectionActive = section;
+  }
 
 }
--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { Dashboard } from '../dashboard/dashboard';
-import { CreateTicket } from '../create-ticket/create-ticket';
 import { Banner } from '../banner/banner';
+import { Dashboard } from '../dashboard/dashboard';
 
 @Component({
   selector: 'app-home',
-  imports: [Dashboard, CreateTicket, Banner],
+  imports: [Banner, Dashboard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
