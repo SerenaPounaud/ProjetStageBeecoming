0258d997ef2acb10aa8d58ebe300d7553521c8f9
SerenaPounaud <Serena.Pounaud@outlook.fr>
2026-06-30 11:03:46 +0200

    commentaire + nettoyage css

--- frontend/src/app/services/auth-interceptor.ts
+++ frontend/src/app/services/auth-interceptor.ts
@@ -1,5 +1,6 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
+//modifie toutes les requêtes HTTP sortantes pour y ajouter un token d’authen si celui-ci existe
 export const authInterceptor: HttpInterceptorFn = (req,next) => {
   const token = localStorage.getItem('token');
   if(!token) {
